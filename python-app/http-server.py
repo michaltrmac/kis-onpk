e9998a32838640e0dec1a363fdd68e7ca040497a
Michal Trmac <mtrmac@koop.cz>
2023-10-24 09:13:53 +0200

    simple python app

---
+++ python-app/http-server.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# ------------------------------------------------------------
+#  Copyright (c) 2023 AIS Servis, s.r.o.
+#  This software is the proprietary information of AIS Servis, s.r.o.
+#  All Right Reserved.
+#  ------------------------------------------------------------
+#
+import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from datetime import datetime
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("\n".join([
+            f'Host: {socket.gethostname()}',
+            f'Time: {datetime.now()}',
+        ]) + '\n', 'utf-8'))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
